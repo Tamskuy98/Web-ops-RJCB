30760d93804756868bc9654285c63cdeb3fb24c4
tama <aldpratamaa@gmail.com>
2026-06-29 01:57:15 +0700

    Change file migration to postgree

--- business-report-system/backend/prisma/migrations/20260310044057_init/migration.sql
+++ business-report-system/backend/prisma/migrations/20260310044057_init/migration.sql
@@ -1,58 +1,86 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" TEXT NOT NULL DEFAULT 'warehouse',
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "products" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "category" TEXT NOT NULL,
-    "price_cost" REAL NOT NULL,
-    "price_sell" REAL NOT NULL,
+    "price_cost" DOUBLE PRECISION NOT NULL,
+    "price_sell" DOUBLE PRECISION NOT NULL,
     "stock" INTEGER NOT NULL DEFAULT 0,
     "min_stock" INTEGER NOT NULL DEFAULT 5,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "sales" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "product_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "price_sell" REAL NOT NULL,
-    "total" REAL NOT NULL,
-    "profit" REAL NOT NULL,
-    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "sales_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "price_sell" DOUBLE PRECISION NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "profit" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sales_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "incoming_goods" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "product_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
     "supplier" TEXT NOT NULL,
-    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "note" TEXT,
-    CONSTRAINT "incoming_goods_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "incoming_goods_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "restocks" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "product_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "purchase_price" REAL NOT NULL,
+    "purchase_price" DOUBLE PRECISION NOT NULL,
     "supplier" TEXT NOT NULL,
-    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "restocks_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "restocks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "sales"
+ADD CONSTRAINT "sales_product_id_fkey"
+FOREIGN KEY ("product_id") REFERENCES "products"("id")
+ON DELETE RESTRICT
+ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "incoming_goods"
+ADD CONSTRAINT "incoming_goods_product_id_fkey"
+FOREIGN KEY ("product_id") REFERENCES "products"("id")
+ON DELETE RESTRICT
+ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "restocks"
+ADD CONSTRAINT "restocks_product_id_fkey"
+FOREIGN KEY ("product_id") REFERENCES "products"("id")
+ON DELETE RESTRICT
+ON UPDATE CASCADE;
