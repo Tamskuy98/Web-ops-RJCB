e93bea3e1f62438a2b75a17ef4b322c02b5c14fb
tama <aldpratamaa@gmail.com>
2026-07-20 23:34:21 +0700

    FIX BE OperationalService add all payment debt

--- business-report-system/backend/src/services/operationalService.js
+++ business-report-system/backend/src/services/operationalService.js
@@ -12,70 +12,78 @@ const createoperational = async (payload = {}) => {
     const data = payload?.data ?? payload;
     const items = Array.isArray(data?.items) ? data.items : [];
 
-    if (!data?.typePayment) {
-      throw new Error("typePayment is required");
+    if (!data) {
+      throw new Error("Payload is required");
     }
     if (items.length === 0) {
       throw new Error("items cannot be empty");
     }
 
     //HANDLE ALLQTY
-    const totalQuantity = items.reduce(
-      (sum, item) => sum + Number(item.qty || 0),
-      0,
+    //HANDLE TOTALPAYMENT
+    const totalItem = items.reduce(
+      (acc, item) => {
+        acc.qty += Number(item.qty || 0);
+        acc.totalPrice += Number(item.totalPrice || 0);
+        return acc;
+      },
+      {
+        qty: 0,
+        totalPrice: 0,
+      },
     );
+
+    const totalQuantity = totalItem.qty;
+    const totalPayment = totalItem.totalPrice;
 
     if (Number(totalQuantity) !== Number(data.allQty)) {
       return "All Quantity not Match";
     }
-
-    //HANDLE TYPEPAYMENT
-    const tempType = [];
-    if (Number(data.cashOnHand) > 0) tempType.push("CASH ON HAND");
-    if (Number(data.cashHold) > 0) tempType.push("CASH HOLD");
-    if (Number(data.qris) > 0) tempType.push("QRIS");
-    const typePayment = tempType.join(";");
-
-    //HANDLE TOTALPAYMENT
-    const totalPayment = items.reduce(
-      (sum, item) => sum + Number(item.totalPrice || 0),
-      0,
-    );
 
     if (Number(totalPayment) !== Number(data.totalPayment)) {
       return "Total Payment not Match";
     }
 
     //HANDLE CASH PAYMENT (CASHONHAND, CASHHOLD, QRIS)
-    const cashOnHand = Number(data.cashOnHand);
-    const cashHold = Number(data.cashHold);
-    const qris = Number(data.qris);
+    const cashOnHand = Number(data.cashOnHand || 0);
+    const cashHold = Number(data.cashHold || 0);
+    const qris = Number(data.qris || 0);
 
-    //HANDLE STATUS
-    const status = data.outstandingPayment > 0 ? "HUTANG" : "LUNAS";
+    //HANDLE TYPEPAYMENT
+    const typePayment =
+      [
+        cashOnHand > 0 && "CASH ON HAND",
+        cashHold > 0 && "CASH HOLD",
+        qris > 0 && "QRIS",
+      ]
+        .filter(Boolean)
+        .join(";") || "DIBAYAR DENGAN HUTANG";
 
     //SUPLIER ON FE
     //DATE ON PRISMA
     //ATTACHEMENT ON FE HOLD
 
     //ATTACHMENT TYPE
-    const attachmentType =
-      data.attachementType === "FOTO" || "foto"
-        ? "FOTO"
-        : data.attachementType === "URL" || "url"
-          ? "URL"
-          : null;
+    const type = data.attachementType?.toUpperCase();
+    const attachmentType = type === "FOTO" || type === "URL" ? type : null;
 
     //HANDLE NOTE ON FE
 
     //HANDLE CATEGORY
-    const category = data?.category === null ? "OPERATIONAL" : "OTHER";
+    const category = data.category ?? "OPERATIONAL";
 
     //HANDLE OUTSTANDINGPAY
-    const outstandingPay = totalPayment - (cashOnHand + cashHold + qris);
+    const outstandingPay = Math.max(
+      totalPayment - (cashOnHand + cashHold + qris),
+      0,
+    );
+
+    // const outstandingPay = totalPayment - (cashOnHand + cashHold + qris);
     if (Number(data.outstandingPay) !== Number(outstandingPay)) {
       return "Outstanding Payment not Match";
     }
+    //HANDLE STATUS
+    const status = outstandingPay > 0 ? "HUTANG" : "LUNAS";
 
     const result = await prisma.$transaction(async (tx) => {
       // Create operational record
