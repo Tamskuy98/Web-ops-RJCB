163e6add47c4544ff68600173d94544bd5b7f556
tama <aldpratamaa@gmail.com>
2026-07-27 22:49:49 +0700

    BUILD BE Deposit

--- business-report-system/backend/src/app.js
+++ business-report-system/backend/src/app.js
@@ -11,6 +11,7 @@ const reportRoutes = require("./routes/reportRoutes");
 const exportRoutes = require("./routes/exportRoutes");
 const dailyReportRoutes = require("./routes/dailyReportRoutes");
 const debtRoutes = require("./routes/debtRoutes");
+const depositRoutes = require("./routes/depositRoutes");
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use("/api/report", reportRoutes);
 app.use("/api/export", exportRoutes);
 app.use("/api/daily-report", dailyReportRoutes);
 app.use("/api/debt-management", debtRoutes);
+app.use("/api/deposit", depositRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ success: false, message: "Endpoint not found" });
